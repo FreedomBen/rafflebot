68c45481fc92e3b29ca921995404aa7baa817c21
Benjamin Porter <bporter@instructure.com>
2015-04-15 14:07:42 -0600

    Use SecureRandom to select a winner

--- bots/rafflebot.rb
+++ bots/rafflebot.rb
@@ -1,8 +1,11 @@
+require 'securerandom'
+
 class RaffleBot < SlackbotFrd::Bot
   def add_callbacks(slack_connection)
     slack_connection.on_message do |user, channel, message|
       if message =~ /^rafflebot/i
-        winner = slack_connection.users_in_channel(channel).sample
+        users = slack_connection.users_in_channel(channel)
+        winner = users[SecureRandom.random_number(users.length)]
         slack_connection.send_message_as_user(channel, "@#{winner} wins!!!", "Raffle Bot", ":raphael:", true)
       end
     end
